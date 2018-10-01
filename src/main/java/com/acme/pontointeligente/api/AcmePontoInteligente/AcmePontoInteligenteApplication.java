ddfe029b9ac80144eaba9cce15edbe73ab0cf43a
Ivan Leiva <leivagarcia18@gmail.com>
2018-10-01 16:50:16 -0500

    Initial Commit

---
+++ src/main/java/com/acme/pontointeligente/api/AcmePontoInteligente/AcmePontoInteligenteApplication.java
@@ -0,0 +1,12 @@
+package com.acme.pontointeligente.api.AcmePontoInteligente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcmePontoInteligenteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcmePontoInteligenteApplication.class, args);
+    }
+}
